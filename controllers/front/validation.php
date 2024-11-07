2b2ae19cfe597b49de3ff48ca00f04a99b82cf84
Raul Bernal <bernal.raul@gmail.com>
2024-11-07 14:09:25 +0100

    fix version selector doing by CometBFT version & updating the checking URL params

--- controllers/front/validation.php
+++ controllers/front/validation.php
@@ -191,39 +191,45 @@ class cosmospayvalidationModuleFrontController extends ModuleFrontController
         }
     }
     public function checkByMemo() {
-        header('Content-Type: application/json');
-        $cart = $this->context->cart;
-        $returnTrans = cosmosSql::getTransaction($cart->id);
-    
-        // Get the LCD URL from the transaction data
-        $lcdPay = $returnTrans[0]['lcd_pay'];
-    
-        // Obtain node information to determine the Cosmos SDK version
-        $nodeInfoJson = file_get_contents($lcdPay.'/cosmos/base/tendermint/v1beta1/node_info');
-        $nodeInfo = json_decode($nodeInfoJson, true);
-    
-        // Check if the node information was obtained successfully
-        if ($nodeInfo !== null && isset($nodeInfo['application_version']['cosmos_sdk_version'])) {
-            // Extract the Cosmos SDK version and remove the 'v' prefix if it exists
-            $cosmosSdkVersion = ltrim($nodeInfo['application_version']['cosmos_sdk_version'], 'v');
-    
-            // Decide which endpoint to use based on the SDK version
-            if (version_compare($cosmosSdkVersion, '0.50.0', '>=')) {
-                // Use the endpoint for SDK v0.50.x
-                $json = file_get_contents($lcdPay.'/cosmos/tx/v1beta1/txs?query=message.action=%27/cosmos.bank.v1beta1.MsgSend%27&order_by=ORDER_BY_DESC&pagination.limit=10');
+      header('Content-Type: application/json');
+      $cart = $this->context->cart;
+      $returnTrans = cosmosSql::getTransaction($cart->id);
+      
+      // Get info from LCD Node to get the Tendermint/CometBFT version
+      // example $getTx = file_get_contents($lcdUrl . '/cosmos/tx/v1beta1/txs/' . $getTxVar);
+      $nodeInfoJson = file_get_contents($lcdUrl.'/cosmos/base/tendermint/v1beta1/node_info');
+      $nodeInfo = json_decode($nodeInfoJson, true);
+      if ($nodeInfo !== null && isset($nodeInfo['default_node_info']['version'])) {
+        // Get the string
+        $versionString = $nodeInfo['default_node_info']['version'];
+        // Split the string '.' => "v0" "38" "11"
+        $versionParts = explode('.', $versionString);
+
+        if (isset($versionParts[1])) {
+            // Get the minor version and convert to integer
+            $minorVersion = intval($versionParts[1]);
+
+            // Check the minor version and apply the proper endpoint fix
+            if ($minorVersion >= 38) {
+                $paramName = 'query';
             } else {
-                // Use the endpoint for SDK v0.47.x
-                $json = file_get_contents($lcdPay.'/cosmos/tx/v1beta1/txs?events=message.action=%27/cosmos.bank.v1beta1.MsgSend%27&order_by=ORDER_BY_DESC&pagination.limit=10');
+                $paramName = 'events';
             }
         } else {
-            // If unable to determine the SDK version, default to the endpoint for SDK v0.50.x
-            $json = file_get_contents($lcdPay.'/cosmos/tx/v1beta1/txs?query=message.action=%27/cosmos.bank.v1beta1.MsgSend%27&order_by=ORDER_BY_DESC&pagination.limit=10');
+            // If something fails getting the version.. use "query" as default
+            $paramName = 'query';
         }
-    
-        $obj = json_decode($json);
-    
-        echo $json;     
+    } else {
+        // If something fails querying the node.. use "query" as default
+        $paramName = 'query';
     }
+   
+      $json = file_get_contents($returnTrans[0]['lcd_pay'].'/cosmos/tx/v1beta1/txs?'. $paramName .'=message.action=%27/cosmos.bank.v1beta1.MsgSend%27&pagination.limit=10&order_by=2&limit=10');
+      $obj = json_decode($json);
+        
+      echo $json;     
+    }
+
     public function postProcess()
     {
     if (isset($_GET['check'])) {
